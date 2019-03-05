1d698818215f240de9f4b18b939cc7cc031d21d1
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-03-05 07:46:49 -0600

    set question data and responses

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 
+
 class QuestionViewController: UIViewController {
     
   
     var questionIndex = 0
+    var answersChose: [Answer] = []
     
  
     @IBOutlet weak var questionLabel: UILabel!
@@ -31,16 +33,21 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multiLabel3: UILabel!
     @IBOutlet weak var multiLabel4: UILabel!
     
-    
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiswitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
     
+    @IBOutlet weak var rangedSlider: UISlider!
+    
+    
+    
     @IBOutlet weak var questionProgressView: UIProgressView!
     
-    
-//    var questions: [Question] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,12 +56,58 @@ class QuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case singleButton1:
+            answersChose.append(currentAnswers[0])
+        case singleButton2:
+            answersChose.append(currentAnswers[1])
+        case singleButton3:
+            answersChose.append(currentAnswers[2])
+        case singleButton4:
+            answersChose.append(currentAnswers[3])
+        default:
+            break
+        }
+        nextQuestion()
+    }
+    
+    
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChose.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answersChose.append(currentAnswers[1])
+        }
+        if multiswitch3.isOn {
+            answersChose.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answersChose.append(currentAnswers[3])
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        
+        answersChose.append(currentAnswers[index])
+        nextQuestion()
+    }
+    
     func updateUI() {
         singleStackView.isHidden = true
         multipleStackView.isHidden = true
         rangedStackView.isHidden = true
-        
-        
         
         let currentQuestion = questions[questionIndex]
         let currentAnswers = currentQuestion.answers
@@ -85,6 +138,10 @@ class QuestionViewController: UIViewController {
     
     func updateMultipleStack(using answers: [Answer]) {
         multipleStackView.isHidden = false
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiswitch3.isOn = false
+        multiSwitch4.isOn = false
         multiLabel1.text = answers[0].text
         multiLabel2.text = answers[1].text
         multiLabel3.text = answers[2].text
@@ -94,8 +151,19 @@ class QuestionViewController: UIViewController {
 
     func updateRangedStack(using answers: [Answer]){
         rangedStackView.isHidden = false
+        rangedSlider.setValue(0.5, animated: false)
         rangedLabel1.text = answers.first?.text
         rangedLabel2.text = answers.last?.text
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "ResultsSegue", sender: nil)
+        }
     }
     /*
     // MARK: - Navigation
